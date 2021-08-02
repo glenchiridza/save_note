7b7b1971d0e2e5c2c289f579e43cf72a559f24b0
glenchiridza <glenchiridz@gmail.com>
2021-08-02 23:43:08 +0200

    notes initializer

--- app/src/main/java/com/glencconnnect/savenote/DataManager.kt
+++ app/src/main/java/com/glencconnnect/savenote/DataManager.kt
@@ -8,4 +8,24 @@ import java.util.*
 class DataManager {
     val courses = HashMap<String,CourseInfo>()
     val notes = ArrayList<CourseInfo>()
+
+    init {
+        initializeCourses()
+    }
+
+    private fun initializeCourses() {
+        var course = CourseInfo("android_intents","Android Programming With Intents")
+        courses.put(course.courseId, course)
+
+        course = CourseInfo("android_async","Android Async Programming and Service")
+        courses.put(course.courseId,course)
+
+        course = CourseInfo("java_lang","Java Fundamentals: The Java Language")
+        courses.put(course.courseId,course)
+
+        course = CourseInfo("java_core","Java Fundamentals: The Core Platform")
+        courses.put(course.courseId,course)
+
+
+    }
 }
