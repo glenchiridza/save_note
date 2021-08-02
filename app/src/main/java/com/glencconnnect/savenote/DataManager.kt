636fe4a5a40871756707921816c72b05b4191429
glenchiridza <glenchiridz@gmail.com>
2021-08-02 23:36:55 +0200

    initialize datamanager

---
+++ app/src/main/java/com/glencconnnect/savenote/DataManager.kt
@@ -0,0 +1,11 @@
+/**
+ * Created by glenc on Aug 2021
+ **/
+package com.glencconnnect.savenote
+
+import java.util.*
+
+class DataManager {
+    val courses = HashMap<String,CourseInfo>()
+    val notes = ArrayList<CourseInfo>()
+}
