a3389fd73dfebfde2b99c1ebbfecc746546d6091
glenchiridza <glenchiridz@gmail.com>
2021-08-14 00:10:38 +0200

    fix list

--- app/src/main/java/com/glencconnnect/savenote/MainActivity.kt
+++ app/src/main/java/com/glencconnnect/savenote/MainActivity.kt
@@ -4,18 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.content_main.*
+import java.util.*
+
+import kotlin.streams.toList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val m = mutableListOf()
-
         val dm = DataManager()
         val adapterCourses = ArrayAdapter<CourseInfo>(this,
                 android.R.layout.simple_spinner_item,
-                 dm.courses.values.toList())
+                 ArrayList<CourseInfo>(dm.courses.values))
         adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinnerCourses.adapter = adapterCourses
     }
--- app/src/main/java/com/glencconnnect/savenote/NoteListActivity.kt
+++ app/src/main/java/com/glencconnnect/savenote/NoteListActivity.kt
@@ -1,9 +1,11 @@
 package com.glencconnnect.savenote
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_note_list.*
 
 class NoteListActivity : AppCompatActivity() {
 
@@ -12,9 +14,10 @@ class NoteListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_note_list)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        fab.setOnClickListener { view ->
+           val intent = Intent(this@NoteListActivity,MainActivity::class.java)
+            startActivity(intent)
+
         }
     }
 }
