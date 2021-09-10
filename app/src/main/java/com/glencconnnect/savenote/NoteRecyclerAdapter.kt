8104d867ccba5aab99141f7abad7baec17517d98
glenchiridza <glenchiridz@gmail.com>
2021-09-10 22:17:12 +0200

    Adapter class

---
+++ app/src/main/java/com/glencconnnect/savenote/NoteRecyclerAdapter.kt
@@ -0,0 +1,14 @@
+/**
+ * Created by glenc on Sep 2021
+ **/
+package com.glencconnnect.savenote
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteRecyclerAdapter {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
