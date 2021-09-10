8e51b8cf7905cd178f028a48f727796b0766c217
glenchiridza <glenchiridz@gmail.com>
2021-09-10 22:28:26 +0200

    id rename

--- app/src/main/java/com/glencconnnect/savenote/NoteRecyclerAdapter.kt
+++ app/src/main/java/com/glencconnnect/savenote/NoteRecyclerAdapter.kt
@@ -7,6 +7,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class NoteRecyclerAdapter(private val context: Context): RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>(){
@@ -15,7 +16,8 @@ class NoteRecyclerAdapter(private val context: Context): RecyclerView.Adapter<No
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView = layoutInflater.inflate(R.layout.item_note_list,parent,false)
+        val itemView = layoutInflater.inflate(R.layout.item_note_list,parent,false)\
+        return ViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -28,5 +30,7 @@ class NoteRecyclerAdapter(private val context: Context): RecyclerView.Adapter<No
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+        val textCourse = itemView.findViewById<TextView>(R.id.textCourse)
+        val textTitle = itemView.findViewById<TextView>(R.id.textTitle)
     }
 }
