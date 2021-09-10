bcb44f0db31f7b77722ad3eaf952d6204af787a7
glenchiridza <glenchiridz@gmail.com>
2021-09-10 22:32:59 +0200

    return values

--- app/src/main/java/com/glencconnnect/savenote/NoteRecyclerAdapter.kt
+++ app/src/main/java/com/glencconnnect/savenote/NoteRecyclerAdapter.kt
@@ -10,23 +10,24 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class NoteRecyclerAdapter(private val context: Context): RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>(){
+class NoteRecyclerAdapter(private val context: Context, private val notes: List<NoteInfo>): RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>(){
 
     private val layoutInflater = LayoutInflater.from(context)
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView = layoutInflater.inflate(R.layout.item_note_list,parent,false)\
+        val itemView = layoutInflater.inflate(R.layout.item_note_list,parent,false)
         return ViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val note = notes[position]
 
+        holder.textCourse.text = note.course?.courseTitle
+        holder.textTitle.text = note.title
     }
 
-    override fun getItemCount(): Int {
-
-    }
+    override fun getItemCount(): Int = notes.size
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
