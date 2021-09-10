1076cfe46e09c76cebb49262519fdf5fb070b2fe
glenchiridza <glenchiridz@gmail.com>
2021-09-10 22:18:29 +0200

    control methods

--- app/src/main/java/com/glencconnnect/savenote/NoteRecyclerAdapter.kt
+++ app/src/main/java/com/glencconnnect/savenote/NoteRecyclerAdapter.kt
@@ -4,11 +4,24 @@
 package com.glencconnnect.savenote
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class NoteRecyclerAdapter {
+class NoteRecyclerAdapter: RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>(){
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
 
     }
 }
